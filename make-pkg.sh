da3cda6a0c3dfe53afc3c88596009e3336a59cbb
Mirek Kratochvil <exa.exa@gmail.com>
2022-06-25 23:49:15 +0200

    v000001

--- make-pkg.sh
+++ make-pkg.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# input environment (+ example)
+# This needs a debian with some tools, such as debhelper and devscripts.
+#
+# Packages are created in this directory. Clean the directory before building
+# the same version again, otherwise weird stuff happens.
+#
+# Input environment (+ example):
 # VERSION (1.6.3)
 # JULIA_ARCH (x86_64)
 # JULIA_ARCH_DIR (x64)
 # DEB_ARCH (amd64)
 #
-# derived automatically but can be overridden:
+# Derived automatically that can be overridden automatically
 # VERSION_BRANCH (1.6)
 # DEB_VERSION (1.8.0rc1)
 #
 # e.g.
-# VERSION=1.7.2 DEB_ARCH=amd64 JULIA_ARCH=x86_64 JULIA_ARCH_DIR=x64 ./make-pkg.sh
+# VERSION=1.7.3 DEB_ARCH=amd64 JULIA_ARCH=x86_64 JULIA_ARCH_DIR=x64 ./make-pkg.sh
+#
+# Again, do not forget to remove tempfiles before each build.
 
 VERSION_BRANCH=${VERSION_BRANCH-$(echo $VERSION | cut -d. -f1-2)}
 DEB_VERSION=${DEB_VERSION-$(echo $VERSION | tr -d -)}
